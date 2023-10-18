9a6bafec38930671969072d97307781ec8bba0c8
Chandler Abril <abrilchandler1@gmail.com>
2023-10-18 12:36:35 -0700

    needed lots of help

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// const titleCased = tutorials.map((item) => {
+//   return titleCased
+// });
+
+//function titleCased() {
+//  const titles = tutorials.map((t) => t.split(" ")) // first step turn each sentence into an array of words
+  // console.log(titles, "titles") // I'm trying to figure out the difference between titles and what I'm doing with words
+ // const words = titles.map((words) => { // if we get disconnected I'll work on some notes and just send them to you later in a replit or something
+   // words.map((w) => {
+
+     //  console.log(w.charAt(0).toUpperCase() + w.slice(1))
+
+  //   }
+ //   )
+ // })
+
+ 
+//  }
+//i'm still working right now and won't get a chance to look at this until later tonight
+
+function titleCased() {
+const titles = tutorials.map((tutorials) => tutorials.split(" "))
+
+
+  const capitalizeTitles = titles.map(tutorial => tutorial.map((word) => word.charAt(0).toUpperCase() + word.slice(1)) )
+ 
+
+ const joinedCapitalizeTitles = capitalizeTitles.map((tutorials) => tutorials.join(" "))
+ return joinedCapitalizeTitles
+} 
